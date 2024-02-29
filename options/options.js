12af61d070a8d4609d14e0a8986b9d1734c2858f
Crowdbotics <engineering@crowdbotics.com>
2024-02-29 06:14:06 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "flex2-dev-134121",
+  url: "https://flex2-dev-134121.botics.co",
+  api: "https://flex2-dev-134121.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
